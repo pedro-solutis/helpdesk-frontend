0fa050bbc4229d3b8bcda3a709b3da95b8571503
Pedro Paulo Silva Santana Ferreira <pedro.ferreira@solutis.com.br>
2026-09-22 12:25:55 -0300

    feat(api): setup axios instance with base url and jwt auth interceptor

---
+++ src/services/api.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+
+const api = axios.create({
+  baseURL: import.meta.env.VITE_BASE_URL || 'http://localhost:8080/api',
+});
+
+api.interceptors.request.use((config) => {
+  const token = sessionStorage.getItem('token');
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+}, (error) => {
+  return Promise.reject(error);
+});
+
+export default api;
+
