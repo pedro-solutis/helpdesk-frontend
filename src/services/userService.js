1639b2cd48ad4c4c5ecaf7d55d61579c3bc72fe0
Pedro Paulo Silva Santana Ferreira <pedro.ferreira@solutis.com.br>
2026-09-23 00:30:47 -0300

    feat(user): add getTechnicians method to user service

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,26 +1,37 @@
 import api from './api';
 
 export const userService = {
+  // GET /users
   getUsers: async (page = 0, size = 10) => {
     const response = await api.get('/users', { params: { page, size } });
     return response.data;
   },
 
+  // GET /users/{id}
   getUserById: async (id) => {
     const response = await api.get(`/users/${id}`);
     return response.data;
   },
 
+  // GET /users/technicians
+  getByTechnicians: async (page = 0, size = 10) => {
+    const response = await api.get('/users/technicians', { params: { page, size }});
+    return response.data;
+  },
+
+  // POST /users
   createUser: async (userData) => {
     const response = await api.post('/users', userData);
     return response.data;
   },
 
+  // PUT /users/{id}
   updateUser: async (id, userData) => {
     const response = await api.put(`/users/${id}`, userData);
     return response.data;
   },
 
+  // DELETE /users/{id}
   deleteUser: async (id) => {
     const response = await api.delete(`/users/${id}`);
     return response.data;
